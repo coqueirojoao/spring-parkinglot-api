606c6c247368225aab9abd40f7913406c5f3e039
João Pedro <jaaum.p95@gmail.com>
2024-03-27 11:07:54 -0300

    :sparkles feat: cria recurso para buscar usuario por id

--- src/main/java/com/coqueiro/demoparkapi/web/controller/UserController.java
+++ src/main/java/com/coqueiro/demoparkapi/web/controller/UserController.java
@@ -5,10 +5,7 @@ import com.coqueiro.demoparkapi.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -22,5 +19,12 @@ public class UserController {
         User createdUser = userService.save(user);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getById(@PathVariable Long id) {
+        User user = userService.findById(id);
+
+        return ResponseEntity.status(HttpStatus.OK).body(user);
     }
 }
