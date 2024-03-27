2fd561770320ef6349bcd0259636a6596e291408
João Pedro <jaaum.p95@gmail.com>
2024-03-27 10:46:17 -0300

    :sparkles feat: cria recurso para adicionar novo usuario

--- src/main/java/com/coqueiro/demoparkapi/service/UserService.java
+++ src/main/java/com/coqueiro/demoparkapi/service/UserService.java
@@ -1,8 +1,10 @@
 package com.coqueiro.demoparkapi.service;
 
+import com.coqueiro.demoparkapi.entity.User;
 import com.coqueiro.demoparkapi.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
@@ -10,5 +12,8 @@ public class UserService {
 
     private final UserRepository userRepository;
 
-
+    @Transactional
+    public User save(User user) {
+        return userRepository.save(user);
+    }
 }
--- src/main/java/com/coqueiro/demoparkapi/web/controller/UserController.java
+++ src/main/java/com/coqueiro/demoparkapi/web/controller/UserController.java
@@ -1,7 +1,12 @@
 package com.coqueiro.demoparkapi.web.controller;
 
+import com.coqueiro.demoparkapi.entity.User;
 import com.coqueiro.demoparkapi.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +16,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
     private final UserService userService;
+
+    @PostMapping
+    public ResponseEntity<User> create(@RequestBody User user) {
+        User createdUser = userService.save(user);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
+    }
 }
